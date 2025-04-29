a99c6301cf8c818393eb5e5e3745ce67436454b8
Abraham Konchellah <abrahamkonchellah@gmail.com>
2025-04-29 10:36:34 +0000

    Using "if", 'else'

---
+++ 3.java
@@ -0,0 +1,16 @@
+class Numberline{
+    void checkNumber(){
+    int number = -5;
+
+        if (number > 0) {
+            System.out.println(" It's a positive number");
+        } else {
+            System.out.println(" It's a Zero or negative number");
+        }
+    
+    
+    public static void main (String[]args){
+        (new Numberline()).checkNumber();
+    }
+}
+}
