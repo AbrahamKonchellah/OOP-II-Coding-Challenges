977b11a30bc37956a666009584ec85f2c1192bd3
Abraham Konchellah <abrahamkonchellah@gmail.com>
2025-04-29 10:33:12 +0000

    Call a function to display a message on the screen

---
+++ 1.java
@@ -0,0 +1,10 @@
+//Output message on the screen 
+public class Hello{ //Class declaration
+
+    void greet(){   //Method declaration
+System.out.println("Hello!");
+    }
+    public static void main(String[] args){
+        (new Hello()).greet();
+    }
+}
