f00548b6f401bd7781bee0025323a4098682b9c0
Abraham Konchellah <abrahamkonchellah@gmail.com>
2025-04-29 10:35:03 +0000

    Using variables

---
+++ 2.java
@@ -0,0 +1,15 @@
+class Person{
+    void showDetails() {
+String name;
+int age;
+
+name= "Abraham";
+System.out.println("Name:"+ name);
+
+age=20;
+System.out.println("Age:" + age);
+
+    }
+public static void main(String[]args);
+(new Person()).showDetails();
+}
